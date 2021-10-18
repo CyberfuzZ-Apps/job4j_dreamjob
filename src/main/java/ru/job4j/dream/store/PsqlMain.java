2ac6e7efdc514940ba3c475e54cd0d0a4ab5254f
CyberfuzZ <keepthelight@icloud.com>
2021-10-18 16:47:07 +0300

    3. Расширить Store для пользователя. [#283109 #217508]

--- src/main/java/ru/job4j/dream/store/PsqlMain.java
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -1,7 +1,6 @@
 package ru.job4j.dream.store;
 
-import ru.job4j.dream.model.Candidate;
-import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 
 /**
  * Класс PsqlMain
@@ -12,21 +11,21 @@ import ru.job4j.dream.model.Post;
 public class PsqlMain {
     public static void main(String[] args) {
         Store store = PsqlStore.instOf();
-        store.save(new Post(0, "Java Job"));
-        for (Post post : store.findAllPosts()) {
-            System.out.println(post.getId() + " " + post.getName());
+        User user = new User(
+                0,
+                "Mark",
+                "mark@mail.ru",
+                "12345"
+        );
+        store.save(user);
+        for (User u : store.findAllUsers()) {
+            System.out.println(u.getId() + " "
+                    + u.getName() + " "
+                    + u.getEmail() + " "
+                    + u.getPassword());
         }
-        store.save(new Candidate(0, "First Candidate"));
-        for (Candidate candidate : store.findAllCandidates()) {
-            System.out.println(candidate.getId() + " " + candidate.getName());
-        }
-        /* update candidate */
-        store.save(new Candidate(1, "Second Candidate"));
-        for (Candidate candidate : store.findAllCandidates()) {
-            System.out.println(candidate.getId() + " " + candidate.getName());
-        }
-        System.out.println("***");
-        System.out.println(store.findPostById(1));
-        System.out.println(store.findCandidateById(1));
+        System.out.println("*****");
+        System.out.println(store.findUserById(1));
+        System.out.println(store.findUserByEmail("mark@mail.ru"));
     }
 }
--- src/main/java/ru/job4j/dream/store/PsqlStore.java
+++ src/main/java/ru/job4j/dream/store/PsqlStore.java
@@ -102,7 +102,23 @@ public class PsqlStore implements Store {
 
     @Override
     public Collection<User> findAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "SELECT * FROM users")
+        ) {
+            try (ResultSet it = ps.executeQuery()) {
+                while (it.next()) {
+                    users.add(new User(it.getInt("id"),
+                            it.getString("name"),
+                            it.getString("email"),
+                            it.getString("password")));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Database error:", e);
+        }
+        return users;
     }
 
     @Override
@@ -202,7 +218,53 @@ public class PsqlStore implements Store {
 
     @Override
     public void save(User user) {
+        if (user.getId() == 0) {
+            create(user);
+        } else {
+            update(user);
+        }
+    }
 
+    private User create(User user) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "INSERT INTO users(name, email, password) VALUES (?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    user.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Database error:", e);
+        }
+        return user;
+    }
+
+    private boolean update(User user) {
+        boolean result = false;
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.setInt(4, user.getId());
+            result = ps.executeUpdate() > 0;
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    user.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Database error:", e);
+        }
+        return result;
     }
 
     @Override
@@ -249,11 +311,47 @@ public class PsqlStore implements Store {
 
     @Override
     public User findUserById(int id) {
-        return null;
+        User user = null;
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "SELECT * FROM users WHERE id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    user = new User(
+                            id,
+                            it.getString("name"),
+                            it.getString("email"),
+                            it.getString("password")
+                    );
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Database error:", e);
+        }
+        return user;
     }
 
     @Override
     public User findUserByEmail(String email) {
-        return null;
+        User user = null;
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "SELECT * FROM users WHERE email = ?")) {
+            ps.setString(1, email);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    user = new User(
+                            it.getInt("id"),
+                            it.getString("name"),
+                            it.getString("email"),
+                            it.getString("password")
+                    );
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Database error:", e);
+        }
+        return user;
     }
 }
