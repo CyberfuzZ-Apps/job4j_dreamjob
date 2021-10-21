7a73805bcbd2f0e5d881cadaef91cde95d61ee98
CyberfuzZ <keepthelight@icloud.com>
2021-10-21 15:44:14 +0300

    4.1. JSON формат. [#238554]

---
+++ src/main/java/ru/job4j/dream/model/Email.java
@@ -0,0 +1,19 @@
+package ru.job4j.dream.model;
+
+/**
+ * Класс Email
+ *
+ * @author Evgeniy Zaytsev
+ * @version 1.0
+ */
+public class Email {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
